8c47792d2ddcc325c21c1b6f96244348495b9bd3
FormulujMario <msh89.mail@gmail.com>
2023-11-13 14:34:07 +0100

    add ProductsSlider module

--- src/components/kabinet-products/KabinetProducts.js
+++ src/components/kabinet-products/KabinetProducts.js
@@ -1,9 +1,9 @@
-import { useRef } from "react";
 import {
   KABINET_PRODUCTS_BEERS_LIST,
   KABINET_PRODUCTS_MERCH_LIST,
   KABINET_PRODUCTS_PIVOLADA_LIST,
 } from "../constants.js";
+import ProductsSlider from "../slider/ProductsSlider.js";
 import "./KabinetProducts.css";
 
 const sliderParams = {
@@ -13,53 +13,6 @@ const sliderParams = {
   direction: "horizontal",
   loop: "false",
   spaceBetween: "30",
-};
-
-const Product = (element) => {
-  return (
-    <swiper-slide>
-      <div className="swiper-slide-item">
-        <img src={element.img} alt="" />
-        <div className="swiper-slide-item-title">
-          <h4>{element.name}</h4>
-          <h4>{element.price}</h4>
-        </div>
-        <p>{element.description}</p>
-      </div>
-    </swiper-slide>
-  );
-};
-
-const ShopNow = (element) => {
-  return (
-    <swiper-slide>
-      <div className="swiper-slide-item-shop-now">
-        <div className="swiper-slide-item-shop-now-img">
-          <img src={element.img} alt=""></img>
-        </div>
-        <span className="shop-now">{element.name}</span>
-      </div>
-    </swiper-slide>
-  );
-};
-
-const SliderForAll = (list) => {
-  const swiperElRef = useRef(null);
-  return (
-    <swiper-container
-      ref={swiperElRef}
-      slides-per-view={sliderParams.slidesPerView}
-      navigation={sliderParams.navigation}
-      pagination={sliderParams.pagination}
-      direction={sliderParams.direction}
-      loop={sliderParams.loop}
-      space-between={sliderParams.spaceBetween}
-    >
-      {list.map((element) => {
-        return element.price ? Product(element) : ShopNow(element);
-      })}
-    </swiper-container>
-  );
 };
 
 const KabinetProducts = () => {
@@ -85,19 +38,25 @@ const KabinetProducts = () => {
       </div>
 
       <div className="swiper swiper-kabinet-products-beers visible">
-        <div className="swiper-wrapper">{SliderForAll(beersList)}</div>
+        <div className="swiper-wrapper">
+          {ProductsSlider(beersList, sliderParams)}
+        </div>
         <div className="swiper-button-prev"></div>
         <div className="swiper-button-next"></div>
       </div>
 
       <div className="swiper swiper-kabinet-products-merch hidden">
-        <div className="swiper-wrapper">{SliderForAll(merchList)}</div>
+        <div className="swiper-wrapper">
+          {ProductsSlider(merchList, sliderParams)}
+        </div>
         <div className="swiper-button-prev"></div>
         <div className="swiper-button-next"></div>
       </div>
 
       <div className="swiper swiper-kabinet-products-pivolada hidden">
-        <div className="swiper-wrapper">{SliderForAll(pivoladaList)}</div>
+        <div className="swiper-wrapper">
+          {ProductsSlider(pivoladaList, sliderParams)}
+        </div>
         <div className="swiper-button-prev"></div>
         <div className="swiper-button-next"></div>
       </div>
---
+++ src/components/slider/ProductsSlider.js
@@ -0,0 +1,51 @@
+import { useRef } from "react";
+import "./ProductsSlider.css";
+
+const product = (element) => {
+  return (
+    <swiper-slide>
+      <div className="swiper-slide-item">
+        <img src={element.img} alt="" />
+        <div className="swiper-slide-item-title">
+          <h4>{element.name}</h4>
+          <h4>{element.price}</h4>
+        </div>
+        <p>{element.description}</p>
+      </div>
+    </swiper-slide>
+  );
+};
+
+const shopNow = (element) => {
+  return (
+    <swiper-slide>
+      <div className="swiper-slide-item-shop-now">
+        <div className="swiper-slide-item-shop-now-img">
+          <img src={element.img} alt=""></img>
+        </div>
+        <span className="shop-now">{element.name}</span>
+      </div>
+    </swiper-slide>
+  );
+};
+
+const ProductsSlider = (list, sliderParams) => {
+  const swiperElRef = useRef(null);
+  return (
+    <swiper-container
+      ref={swiperElRef}
+      slides-per-view={sliderParams.slidesPerView}
+      navigation={sliderParams.navigation}
+      pagination={sliderParams.pagination}
+      direction={sliderParams.direction}
+      loop={sliderParams.loop}
+      space-between={sliderParams.spaceBetween}
+    >
+      {list.map((element) => {
+        return element.price ? product(element) : shopNow(element);
+      })}
+    </swiper-container>
+  );
+};
+
+export default ProductsSlider;
