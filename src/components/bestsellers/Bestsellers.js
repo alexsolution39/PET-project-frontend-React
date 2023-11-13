9ab9e44a883096eb15bf581c1168a3f7aaba3a2d
FormulujMario <msh89.mail@gmail.com>
2023-11-13 14:39:23 +0100

    update Bestsellers module

--- src/components/bestsellers/Bestsellers.js
+++ src/components/bestsellers/Bestsellers.js
@@ -1,9 +1,18 @@
-import { useRef } from "react";
 import { BESTSELLERS_LIST } from "../constants.js";
 import NormalWhiteButton from "../buttons/NormalWhiteButton.js";
 import { BsArrowUpRight } from "react-icons/bs";
+import ProductsSlider from "../slider/ProductsSlider.js";
+
+const sliderParams = {
+  slidesPerView: "4",
+  navigation: "true",
+  pagination: "false",
+  direction: "horizontal",
+  loop: "false",
+  spaceBetween: "30",
+};
+
 const Bestsellers = () => {
-  const swiperElRef = useRef(null);
   const bestsellers = BESTSELLERS_LIST;
 
   return (
@@ -15,30 +24,7 @@ const Bestsellers = () => {
       <div className="bestsellers-products">
         <div className="swiper swiper-bestsellers">
           <div className="swiper-wrapper">
-            <swiper-container
-              ref={swiperElRef}
-              slides-per-view="4"
-              navigation="true"
-              pagination="false"
-              direction="horizontal"
-              loop="false"
-              space-between="30"
-            >
-              {bestsellers.map((element) => {
-                return (
-                  <swiper-slide>
-                    <div className="swiper-slide-item">
-                      <img src={element.img} alt="" />
-                      <div className="swiper-slide-item-title">
-                        <h4>{element.name}</h4>
-                        <h4>{element.price}</h4>
-                      </div>
-                      <p>{element.description}</p>
-                    </div>
-                  </swiper-slide>
-                );
-              })}
-            </swiper-container>
+            {ProductsSlider(bestsellers, sliderParams)}
           </div>
           <div className="swiper-button-prev"></div>
           <div className="swiper-button-next"></div>
