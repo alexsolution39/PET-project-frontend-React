03684609afc5b336e790f390d062d42ea9ce1937
FormulujMario <msh89.mail@gmail.com>
2023-11-16 10:08:10 +0100

    add NormalTransparentButton module

---
+++ src/components/buttons/NormalTransparentButton.js
@@ -0,0 +1,12 @@
+import './NormalTransparentButton.css'
+
+function NormalTransparentButton({text, icon}) {
+  return (
+    <button className="normal-button transparent-button">
+      {text}
+      {icon}
+    </button>
+  );
+}
+
+export default NormalTransparentButton;
