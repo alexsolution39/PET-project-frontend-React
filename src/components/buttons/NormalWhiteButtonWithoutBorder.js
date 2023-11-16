d08fdaeab0404c1b61e2740beab697cd6a3ad43a
FormulujMario <msh89.mail@gmail.com>
2023-11-16 10:16:02 +0100

    add NormalWhiteButtonWithoutBorder module

---
+++ src/components/buttons/NormalWhiteButtonWithoutBorder.js
@@ -0,0 +1,12 @@
+import './NormalWhiteButtonWithoutBorder.css'
+
+function NormalWhiteButtonWithoutBorder({text, icon}) {
+  return (
+    <button className="normal-button white-button-without-border">
+      {text}
+      {icon}
+    </button>
+  );
+}
+
+export default NormalWhiteButtonWithoutBorder;
