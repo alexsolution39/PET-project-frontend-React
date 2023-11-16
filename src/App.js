57f1dd516ddd3468d284ff40a1d368e65e83d29e
FormulujMario <msh89.mail@gmail.com>
2023-11-16 10:17:03 +0100

    add KabinetLounge module

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SliderMainPage from "./components/slider/SliderMainPage.js";
 import Bestsellers from "./components/bestsellers/Bestsellers.js";
 import LinkToAboutUs from "./components/link-to-about-us/LinkToAboutUs.js";
 import KabinetProducts from "./components/KabinetProducts.js";
+import KabinetLounge from "./components/kabinet-lounge/KabinetLounge.js";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Bestsellers />
         <LinkToAboutUs />
         <KabinetProducts/>
+        <KabinetLounge/>
       </main>
     </div>
   );
