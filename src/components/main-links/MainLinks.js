bcef89c027e9a5a3937cb0d764b0a68645816808
FormulujMario <msh89.mail@gmail.com>
2023-11-19 11:42:02 +0100

    add MainLinks module

---
+++ src/components/main-links/MainLinks.js
@@ -0,0 +1,31 @@
+import { FOOTER_MENU_LIST } from "../constants";
+import { SOC_NET_ICONS } from "../constants";
+import "./MainLinks.css";
+
+const MainLinks = () => {
+  return (
+    <section className="main-links-footer">
+      <div className="list-menu-footer">
+        {FOOTER_MENU_LIST.map((element) => {
+          return (
+            <div>
+              <span>{element.name}</span>
+              <ul>
+                {element.list.map((subelement) => {
+                  return <li>{subelement}</li>;
+                })}
+              </ul>
+            </div>
+          );
+        })}
+      </div>
+      <div className="soc-net-icons">
+        {SOC_NET_ICONS.map((icon) => {
+          return icon.svg;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default MainLinks;
