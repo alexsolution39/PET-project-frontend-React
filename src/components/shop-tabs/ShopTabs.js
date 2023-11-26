c2d08dc0f2115ebaa24650cbc32b9977e90d78c7
FormulujMario <msh89.mail@gmail.com>
2023-11-26 15:35:48 +0100

    add FiltersSection to ShopTabs

--- src/components/shop-tabs/ShopTabs.js
+++ src/components/shop-tabs/ShopTabs.js
@@ -4,15 +4,12 @@ import {
   SHOP_MAIN_BEER_PACKS_LIST,
   SHOP_MAIN_PIVOLADA_LIST,
   SHOP_MAIN_MERCH_LIST,
-  SORT_BY,
-  STYLE,
 } from "../constants.js";
 import ShopCategories from "../ShopCategories.js";
-import "./ShopTabs.css";
 import { useState } from "react";
-import NormalWhiteButton from "../buttons/NormalWhiteButton";
-import { BsArrowRight } from "react-icons/bs";
-import Sort from "../sort/Sort.js";
+import ShowFilters from "../buttons/ShowFilters.js";
+import FiltersSection from "../filters/FiltersSection.js";
+import "./ShopTabs.css";
 
 const sliderParams = {
   slidesPerView: "4",
@@ -83,44 +80,37 @@ const ShopTabs = () => {
                 categories={[
                   {
                     category: "BEER PACKS",
-                    show: beerPacks,
+                    show: SHOP_MAIN_BEER_PACKS_LIST,
                     sliderParams: sliderParams,
                   },
                   {
                     category: "BEERS",
-                    show: beers,
+                    show: SHOP_MAIN_BEERS_LIST,
                     sliderParams: sliderParams,
                   },
                   {
                     category: "PIVOLADA",
-                    show: pivolada,
+                    show: SHOP_MAIN_PIVOLADA_LIST,
                     sliderParams: sliderParams,
                   },
                   {
                     category: "MERCH",
-                    show: merch,
+                    show: SHOP_MAIN_MERCH_LIST,
                     sliderParams: sliderParams,
                   },
                 ]}
               />
             </Tab.Pane>
             <Tab.Pane eventKey="BEER PACKS">
-              <div
-                class="filters-section beers-filter"
-                style={{ display: filters }}
-              >
-                <div class="filters-wrapper">
-                  <Sort
-                    list={SORT_BY}
-                    state={beerPacks}
-                    stateFn={setBeerPacks}
-                    section={"sort-beerpacks"}
-                  />
-                </div>
-                <div class="clear-button">
-                  <button>CLEAR ALL FILTERS</button>
-                </div>
-              </div>
+              <FiltersSection
+                filters={filters}
+                state={beerPacks}
+                stateFn={setBeerPacks}
+                productsList={SHOP_MAIN_BEER_PACKS_LIST}
+                sectionSort={"sort-beerpacks"}
+                sectionFilter={"beer-packs"}
+                quantity={true}
+              />
               <ShopCategories
                 categories={"BEER PACKS"}
                 show={beerPacks}
@@ -128,84 +118,18 @@ const ShopTabs = () => {
               />
             </Tab.Pane>
             <Tab.Pane eventKey="BEERS">
-              <div
-                class="filters-section beers-filter"
-                style={{ display: filters }}
-              >
-                <div class="filters-wrapper">
-                  <Sort
-                    list={SORT_BY}
-                    state={beers}
-                    stateFn={setBeers}
-                    section={"sort-beers"}
-                  />
-                  <div class="style">
-                    <p class="filter-title">Style</p>
-                    {STYLE.map((element) => {
-                      const id = element.replace(/\//g, "-").toLowerCase();
-                      return (
-                        <div>
-                          <input type="checkbox" id={id} name={id} />
-                          <label for={id}>{element}</label>
-                        </div>
-                      );
-                    })}
-                  </div>
-                  <div class="alcohol">
-                    <p class="filter-title">Alcohol</p>
-                    <div>
-                      <input type="checkbox" id="zero-five" name="zero-five" />
-                      <label for="zero-five">0% - 5%</label>
-                    </div>
-                    <div>
-                      <input
-                        type="checkbox"
-                        id="five-seven"
-                        name="five-seven"
-                      />
-                      <label for="five-seven">5% - 7%</label>
-                    </div>
-                    <div>
-                      <input
-                        type="checkbox"
-                        id="over-seven"
-                        name="over-seven"
-                      />
-                      <label for="over-seven">Over 7%</label>
-                    </div>
-                  </div>
-                  <div class="color">
-                    <p class="filter-title">Color</p>
-                    <div>
-                      <input
-                        type="checkbox"
-                        id="to-fifteen-ebc"
-                        name="to-fifteen-ebc"
-                      />
-                      <label for="to-fifteen-ebc">To 15 EBC</label>
-                    </div>
-                    <div>
-                      <input
-                        type="checkbox"
-                        id="fifteen-fourty-ebc"
-                        name="fifteen-fourty-ebc"
-                      />
-                      <label for="fifteen-fourty-ebc">15 - 40 EBC</label>
-                    </div>
-                    <div>
-                      <input
-                        type="checkbox"
-                        id="over-fourty-ebc"
-                        name="over-fourty-ebc"
-                      />
-                      <label for="over-fourty-ebc">Over 40 EBC</label>
-                    </div>
-                  </div>
-                </div>
-                <div class="clear-button">
-                  <button>CLEAR ALL FILTERS</button>
-                </div>
-              </div>
+              <FiltersSection
+                filters={filters}
+                state={beers}
+                stateFn={setBeers}
+                productsList={SHOP_MAIN_BEERS_LIST}
+                sectionSort={"sort-beers"}
+                sectionFilter={"beers"}
+                style={true}
+                alcohol={true}
+                color={true}
+                quantity={false}
+              />
               <ShopCategories
                 categories={"BEERS"}
                 show={beers}
@@ -213,22 +137,14 @@ const ShopTabs = () => {
               />
             </Tab.Pane>
             <Tab.Pane eventKey="PIVOLADA">
-              <div
-                class="filters-section beers-filter"
-                style={{ display: filters }}
-              >
-                <div class="filters-wrapper">
-                  <Sort
-                    list={SORT_BY}
-                    state={pivolada}
-                    stateFn={setPivolada}
-                    section={"sort-pivolada"}
-                  />
-                </div>
-                <div class="clear-button">
-                  <button>CLEAR ALL FILTERS</button>
-                </div>
-              </div>
+              <FiltersSection
+                filters={filters}
+                state={pivolada}
+                stateFn={setPivolada}
+                productsList={SHOP_MAIN_PIVOLADA_LIST}
+                sectionSort={"sort-pivolada"}
+              />
+
               <ShopCategories
                 categories={"PIVOLADA"}
                 show={pivolada}
@@ -236,22 +152,13 @@ const ShopTabs = () => {
               />
             </Tab.Pane>
             <Tab.Pane eventKey="MERCH">
-              <div
-                class="filters-section beers-filter"
-                style={{ display: filters }}
-              >
-                <div class="filters-wrapper">
-                  <Sort
-                    list={SORT_BY}
-                    state={merch}
-                    stateFn={setMerch}
-                    section={"sort-merch"}
-                  />
-                </div>
-                <div class="clear-button">
-                  <button>CLEAR ALL FILTERS</button>
-                </div>
-              </div>
+              <FiltersSection
+                filters={filters}
+                state={merch}
+                stateFn={setMerch}
+                productsList={SHOP_MAIN_MERCH_LIST}
+                sectionSort={"sort-merch"}
+              />
               <ShopCategories
                 categories={"MERCH"}
                 show={merch}
